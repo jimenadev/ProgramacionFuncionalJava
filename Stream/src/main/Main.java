2a9f19629819518e5ab63b2486e333b2e23b5d83
Jimena Silva <jimenasilva@iMac-de-Jimena.local>
2024-08-28 17:17:36 -0400

    filter elements

---
+++ Stream/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Filtrar!
+		//Obtener la cantidad de usuarios con una edad mayor a 18 y que su username comience con R
+		
+		List<User> users = new ArrayList<>();
+		
+		users.add(new User("Rafael", 25));
+		users.add(new User("Daniel", 20));
+		users.add(new User("Tania", 15));
+		users.add(new User("Luisa", 24));
+		users.add(new User("Raul", 21));
+		
+		long cantidad = users.stream().count();
+		System.out.println(cantidad);
+		
+		long userMayor = users.stream()
+							.filter( user -> user.getAge() > 18)
+							.filter( user -> user.getUsername().startsWith("R"))
+							.count();
+		
+		System.out.println(userMayor);
+
+	}
+
+}
